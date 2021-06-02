439c3b7b57ffe3179b613d917e3dbe89604266bd
wbadmin <>
2021-06-02 20:10:16 +0200

    {/src/main/java/com/bikerental/bikerental/User.java}

--- src/main/java/com/bikerental/bikerental/User.java
+++ src/main/java/com/bikerental/bikerental/User.java
@@ -6,12 +6,47 @@ package com.bikerental.bikerental;
 
 public class User implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public User() {
-    }
+	@org.kie.api.definition.type.Label(value = "FirstName")
+	private java.lang.String firstName;
+	@org.kie.api.definition.type.Label(value = "LastName")
+	private java.lang.String lastName;
+	@org.kie.api.definition.type.Label(value = "Email")
+	private java.lang.String email;
 
+	public User() {
+	}
 
+	public java.lang.String getFirstName() {
+		return this.firstName;
+	}
 
+	public void setFirstName(java.lang.String firstName) {
+		this.firstName = firstName;
+	}
+
+	public java.lang.String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(java.lang.String lastName) {
+		this.lastName = lastName;
+	}
+
+	public java.lang.String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(java.lang.String email) {
+		this.email = email;
+	}
+
+	public User(java.lang.String firstName, java.lang.String lastName,
+			java.lang.String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
 
 }
