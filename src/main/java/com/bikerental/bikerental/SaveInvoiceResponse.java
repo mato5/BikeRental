7e9a3d60202a5822e850e330b5304ac6eb63ce87
wbadmin <>
2021-06-06 20:21:37 +0200

    {/src/main/java/com/bikerental/bikerental/SaveInvoiceResponse.java}

--- src/main/java/com/bikerental/bikerental/SaveInvoiceResponse.java
+++ src/main/java/com/bikerental/bikerental/SaveInvoiceResponse.java
@@ -6,12 +6,23 @@ package com.bikerental.bikerental;
 
 public class SaveInvoiceResponse implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public SaveInvoiceResponse() {
-    }
+	private java.lang.String status;
 
+	public SaveInvoiceResponse() {
+	}
 
+	public java.lang.String getStatus() {
+		return this.status;
+	}
 
+	public void setStatus(java.lang.String status) {
+		this.status = status;
+	}
+
+	public SaveInvoiceResponse(java.lang.String status) {
+		this.status = status;
+	}
 
 }
