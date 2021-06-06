db67660ba3f50d3198b6bfdeffd0eecd33b7bbc7
wbadmin <>
2021-06-06 20:23:42 +0200

    {/src/main/java/com/bikerental/bikerental/LogIssueResponse.java}

--- src/main/java/com/bikerental/bikerental/LogIssueResponse.java
+++ src/main/java/com/bikerental/bikerental/LogIssueResponse.java
@@ -6,12 +6,23 @@ package com.bikerental.bikerental;
 
 public class LogIssueResponse implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public LogIssueResponse() {
-    }
+	private java.lang.String status;
 
+	public LogIssueResponse() {
+	}
 
+	public java.lang.String getStatus() {
+		return this.status;
+	}
 
+	public void setStatus(java.lang.String status) {
+		this.status = status;
+	}
+
+	public LogIssueResponse(java.lang.String status) {
+		this.status = status;
+	}
 
 }
